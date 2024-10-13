8e652febbccbf1f4f88be0797a65cdccb7e82ae6
jfloresxc <jfloresxc@gmail.com>
2024-10-13 18:38:00 -0500

    Update - change sections actived

---
+++ src/lib/presentations.ts
@@ -0,0 +1,11 @@
+export interface Presentation {
+    id: number
+    title: string
+    description: string
+    address: string
+    date: string
+}
+
+export const PRESENTATIONS: Presentation[] = [
+
+];
---
+++ src/lib/speakers.ts
@@ -0,0 +1,87 @@
+export interface Speaker {
+    id?: string;
+    speaker: string;
+    profesion: string;
+    url: string;
+    urlLinkedin: string;
+    urlInstragram?: string;
+}
+
+export const SPEAKERS: Speaker[] = [
+    {
+        speaker: "Jose Flores",
+        profesion:
+            "Desarrollador web 🧑🏻‍🚀 | Presidente Área de Programación IEEE CS 🚀 | Ingeniero de Sistemas 🏗️",
+        url: "jose-flores.png",
+        urlLinkedin: "https://www.linkedin.com/in/jfloresxc/",
+    },
+    {
+        speaker: "Carlos Pérez (Charly)",
+        profesion:
+            "Filósofo Práctico y Consultor Ágil en Philosophia Consulting",
+        url: "carlos-perez.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/carpercharly/",
+    },
+    {
+        speaker: "Nefi Arroyo",
+        profesion: "Tech Owner of AWS availability",
+        url: "nefi-arroyo.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/nefiarroyo/",
+    },
+    {
+        speaker: "Antonella Ochoa",
+        profesion: "Project Analyst | PSM I | Customer Experience",
+        url: "antonella-ochoa.jpg",
+        urlLinkedin:
+            "https://www.linkedin.com/in/antonella-ochoa-68564089/",
+    },
+    {
+        speaker: "Yancel Salinas",
+        profesion:
+            "Production Engineer at Zebrands | Lead Google Developer Group Open | SRE | DevOps | Python | Go | GCP | AWS",
+        url: "yancel-salinas.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/sagoyanfisic/",
+    },
+    {
+        speaker: "Emelyn Alvino",
+        profesion:
+            "Corporate Talent Acquisition Analyst at AENZA💙🤍👷🏻‍♀️ | Headhunter | Employer Branding | HR Automation | People Analytics",
+        url: "emelyn-alvino.jpeg",
+        urlLinkedin: "https://www.linkedin.com/in/cristinaalvino/",
+    },
+    {
+        speaker: "Arturo Simich",
+        profesion:
+            "Senior Manager Cibersecurity | Risk Technology Director | Digital Transformation Expert | Startup Mentor",
+        url: "arturo-simich.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/arturosimich/",
+    },
+    {
+        speaker: "Cleber De Cunha",
+        profesion:
+            "DS - GenAI Adoption Lead | Senior Agile Transformation Coach",
+        url: "cleber-de-cunha.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/clebercunha/",
+    },
+    {
+        speaker: "Jorge Luis Rejas",
+        profesion:
+            "C-AI/MLPen | CMPen | LFD121 | CNSP | eMAPT | AppSec | eJPT | CEH-P | SC-300 | SC-900 | AZ-900 | DevSecOps | CSX-F",
+        url: "jorge-luis-rejas.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/jrejas/",
+    },
+    {
+        speaker: "Ricardo Berrospi",
+        profesion:
+            "IT Professional, MSc, MBA with studies in Georgetown University, George Washington University, National University of Engineering (UNI), MIT University, ESAN University",
+        url: "ricardo-berrospi.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/ricardo-berrospit/",
+    },
+    {
+        speaker: "Alfredo Amez ",
+        profesion:
+            "Top Voice: Business Intelligence | +12k | Growth | CRM | Customer Intelligence | Marketing Analytics | Microsoft Certified Trainer 2021 - 2025",
+        url: "alfredo.jpg",
+        urlLinkedin: "https://www.linkedin.com/in/afredoamez/",
+    },
+];
